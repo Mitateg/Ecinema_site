4096e71044d866c9d57cac423eb2cb4d65d0337b
Fuior Vladimir <fuiorvladimir123@gmail.com>
2025-04-05 13:33:16 +0300

    Business logic

---
+++ Ecinema_site.BusinessLogic/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecinema_site.BusinessLogic
+{
+    public class Class1
+    {
+    }
+}
