40cd931d3f674eb4e8c99fbbd48d13bf05f708a2
Catalin100000 <catalin.afteni4@gmail.com>
2025-04-18 16:51:05 +0300

    Controlers creation

---
+++ Ecinema_site.Web/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecinema_site.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Profile()
+        {
+            // Fetch user data (mock example)
+            var user = new { Name = "John Doe", Email = "john.doe@example.com" };
+            return View(user);
+        }
+    }
+}
