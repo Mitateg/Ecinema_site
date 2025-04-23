bee0560a2d9f98a65dcb461d9459ad7f0d196b0b
Catalin100000 <catalin.afteni4@gmail.com>
2025-04-23 16:37:33 +0300

    Controlers creation

---
+++ Ecinema_site.Web/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecinema_site.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Login
+        [HttpPost]
+        public ActionResult Index(string username, string password)
+        {
+            // Add authentication logic here
+            if (username == "admin" && password == "password") // Example logic
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.ErrorMessage = "Invalid username or password.";
+            return View();
+        }
+    }
+}
