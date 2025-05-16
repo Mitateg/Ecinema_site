3d0527ceea0250410c5f71760f717639720f41ac
Catalin100000 <catalin.afteni4@gmail.com>
2025-05-16 20:59:44 +0300

    Movie change, login required

--- Ecinema_site.Domain/Migrations/Configuration.cs
+++ Ecinema_site.Domain/Migrations/Configuration.cs
@@ -36,6 +36,64 @@
                 };
                 userManager.Create(adminUser, "Admin123!");
             }
+
+            // Seed movies if none exist
+            if (!context.Movies.Any())
+            {
+                context.Movies.AddOrUpdate(
+                    m => m.Title,
+                    new Movie
+                    {
+                        Title = "Alien",
+                        Description = "In deep space, the crew of the commercial starship Nostromo is awakened from their cryo-sleep capsules halfway through their journey home to investigate a distress call from an alien vessel.",
+                        Year = 1979,
+                        Genre = "Action, Adventure, Horror",
+                        Rating = 4.5,
+                        PosterUrl = "/Content/Images/Movies/Movie_poster_1.png",
+                        VideoUrl = "/Content/Videos/sample.mp4"
+                    },
+                    new Movie
+                    {
+                        Title = "Fight Club",
+                        Description = "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.",
+                        Year = 1999,
+                        Genre = "Drama, Thriller",
+                        Rating = 4.7,
+                        PosterUrl = "/Content/Images/Movies/Movie_poster_2.png",
+                        VideoUrl = "/Content/Videos/sample.mp4"
+                    },
+                    new Movie
+                    {
+                        Title = "The Goonies",
+                        Description = "A group of young misfits called The Goonies discover an ancient map and set out on an adventure to find a legendary pirate's long-lost treasure.",
+                        Year = 1985,
+                        Genre = "Adventure, Comedy",
+                        Rating = 4.8,
+                        PosterUrl = "/Content/Images/Movies/Movie_poster_3.png",
+                        VideoUrl = "/Content/Videos/sample.mp4"
+                    },
+                    new Movie
+                    {
+                        Title = "The Shawshank Redemption",
+                        Description = "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+                        Year = 1994,
+                        Genre = "Drama",
+                        Rating = 4.9,
+                        PosterUrl = "/Content/Images/Movies/Movie_poster_4.png",
+                        VideoUrl = "/Content/Videos/sample.mp4"
+                    },
+                    new Movie
+                    {
+                        Title = "Interstellar",
+                        Description = "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+                        Year = 2014,
+                        Genre = "Adventure, Drama, Sci-Fi",
+                        Rating = 4.6,
+                        PosterUrl = "/Content/Images/Movies/Movie_poster_5.png",
+                        VideoUrl = "/Content/Videos/sample.mp4"
+                    }
+                );
+            }
         }
     }
 }
--- Ecinema_site.Web/Controllers/MoviesController.cs
+++ Ecinema_site.Web/Controllers/MoviesController.cs
@@ -1,115 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using Ecinema_site.Domain;
+using Ecinema_site.Domain.Entities;
 
 namespace Ecinema_site.Web.Controllers
 {
     public class MoviesController : Controller
     {
-        // Sample movie data - in a real app, this would come from a database
-        private static List<dynamic> _movies = new List<dynamic>
-        {
-            new { 
-                Id = 1, 
-                Title = "Alien", 
-                Description = "In deep space, the crew of the commercial starship Nostromo is awakened from their cryo-sleep capsules halfway through their journey home to investigate a distress call from an alien vessel.", 
-                Year = 1979, 
-                Genre = "Action, Adventure, Horror", 
-                Rating = 4.5, 
-                ImagePath = "~/Content/Images/Movies/Movie_poster_1.png", 
-                VideoPath = "~/Content/Videos/sample.mp4" 
-            },
-            new { 
-                Id = 2, 
-                Title = "Fight Club", 
-                Description = "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.", 
-                Year = 1999, 
-                Genre = "Drama, Thriller", 
-                Rating = 4.7, 
-                ImagePath = "~/Content/Images/Movies/Movie_poster_2.png", 
-                VideoPath = "~/Content/Videos/sample.mp4" 
-            },
-            new { 
-                Id = 3, 
-                Title = "The Goonies", 
-                Description = "A group of young misfits called The Goonies discover an ancient map and set out on an adventure to find a legendary pirate's long-lost treasure.", 
-                Year = 1985, 
-                Genre = "Adventure, Comedy", 
-                Rating = 4.8, 
-                ImagePath = "~/Content/Images/Movies/Movie_poster_3.png", 
-                VideoPath = "~/Content/Videos/sample.mp4" 
-            },
-            new { 
-                Id = 4, 
-                Title = "The Shawshank Redemption", 
-                Description = "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.", 
-                Year = 1994, 
-                Genre = "Drama", 
-                Rating = 4.9, 
-                ImagePath = "~/Content/Images/Movies/Movie_poster_4.png", 
-                VideoPath = "~/Content/Videos/sample.mp4" 
-            },
-            new { 
-                Id = 5, 
-                Title = "Interstellar", 
-                Description = "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.", 
-                Year = 2014, 
-                Genre = "Adventure, Drama, Sci-Fi", 
-                Rating = 4.6, 
-                ImagePath = "~/Content/Images/Movies/Movie_poster_5.png", 
-                VideoPath = "~/Content/Videos/sample.mp4" 
-            }
-        };
+        private EcinemaDbContext db = new EcinemaDbContext();
 
+        // GET: Movies
         public ActionResult Index()
         {
-            // Pass the list of movies to the view
-            return View(_movies);
+            var movies = db.Movies.ToList();
+            return View(movies);
         }
 
-        // GET: /Movies/Details/5
+        // GET: Movies/Details/5
+        [Authorize]
         public ActionResult Details(int id = 0)
         {
-            // Default to first movie if id is 0
-            if (id == 0) id = 1;
-            
-            try
-            {
-                // Find the movie with the specified ID
-                var movie = _movies.FirstOrDefault(m => m.Id == id);
-                
-                // If movie not found, return the first movie as fallback
-                if (movie == null)
-                {
-                    movie = _movies.FirstOrDefault();
-                    if (movie == null)
-                    {
-                        return HttpNotFound("No movies found");
-                    }
-                }
-                
-                // Pass the movie to the view
-                return View(movie);
-            }
-            catch (Exception ex)
-            {
-                // Log the exception
-                System.Diagnostics.Debug.WriteLine($"Error in Details action: {ex.Message}");
-                
-                // Return a default movie if there's an error
-                return View(new { 
-                    Id = id, 
-                    Title = "Movie Details", 
-                    Description = "Movie information could not be loaded.", 
-                    Genre = "N/A", 
-                    Year = 0, 
-                    Rating = 0.0, 
-                    ImagePath = "~/Content/Images/Movies/Movie_poster_1.png",
-                    VideoPath = "~/Content/Videos/sample.mp4"
-                });
-            }
+            if (id == 0)
+                return RedirectToAction("Index");
+
+            var movie = db.Movies.FirstOrDefault(m => m.Id == id);
+            if (movie == null)
+                return HttpNotFound("Movie not found");
+
+            return View(movie);
         }
     }
 }
