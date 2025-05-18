a248578d67fa5d0381105098749f7590e69f94e4
Mitateg <fuiorvladimir123@gmail.com>
2025-05-18 21:10:42 +0300

    working search

--- Ecinema_site.Web/Controllers/SearchController.cs
+++ Ecinema_site.Web/Controllers/SearchController.cs
@@ -3,15 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ecinema_site.Domain;
+using Ecinema_site.Domain.Entities;
 
 namespace Ecinema_site.Web.Controllers
 {
     public class SearchController : Controller
     {
+        private EcinemaDbContext db = new EcinemaDbContext();
+
         // GET: Search
-        public ActionResult Index()
+        public ActionResult Index(string name = null, List<string> genres = null)
         {
-            return View();
+            var allGenres = db.Movies
+                .AsEnumerable()
+                .SelectMany(m => m.Genre.Split(','))
+                .Select(g => g.Trim())
+                .Distinct()
+                .OrderBy(g => g)
+                .ToList();
+
+            var movies = db.Movies.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                movies = movies.Where(m => m.Title.Contains(name));
+            }
+            if (genres != null && genres.Any())
+            {
+                movies = movies.Where(m => genres.Any(g => m.Genre.Contains(g)));
+            }
+
+            ViewBag.AllGenres = allGenres;
+            ViewBag.SelectedGenres = genres ?? new List<string>();
+            ViewBag.SearchName = name;
+
+            return View(movies.ToList());
         }
     }
 }
