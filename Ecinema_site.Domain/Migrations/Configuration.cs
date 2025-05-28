deadabb3252143db73258ef97de496bcf7f1b706
Catalin100000 <catalin.afteni4@gmail.com>
2025-05-28 11:46:15 +0300

    working fix

--- Ecinema_site.Domain/Migrations/Configuration.cs
+++ Ecinema_site.Domain/Migrations/Configuration.cs
@@ -50,17 +50,17 @@
                         Genre = "Action, Adventure, Horror",
                         Rating = 4.5,
                         PosterUrl = "/Content/Images/Movies/Movie_poster_1.png",
-                        VideoUrl = "/Content/Videos/sample.mp4"
+                        VideoUrl = "/Content/Videos/alien.mp4"
                     },
                     new Movie
                     {
-                        Title = "Fight Club",
-                        Description = "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.",
-                        Year = 1999,
-                        Genre = "Drama, Thriller",
+                        Title = "Back to the Future",
+                        Description = "Marty McFly, a 17-year-old high school student, is accidentally sent thirty years into the past in a time-traveling DeLorean invented by his close friend, the maverick scientist Doc Brown.",
+                        Year = 1985,
+                        Genre = "Sci-Fi, Adventure",
                         Rating = 4.7,
                         PosterUrl = "/Content/Images/Movies/Movie_poster_2.png",
-                        VideoUrl = "/Content/Videos/sample.mp4"
+                        VideoUrl = "/Content/Videos/back_to_the_future.mp4"
                     },
                     new Movie
                     {
@@ -70,30 +70,68 @@
                         Genre = "Adventure, Comedy",
                         Rating = 4.8,
                         PosterUrl = "/Content/Images/Movies/Movie_poster_3.png",
-                        VideoUrl = "/Content/Videos/sample.mp4"
+                        VideoUrl = "/Content/Videos/the_goonies.mp4"
                     },
                     new Movie
                     {
-                        Title = "The Shawshank Redemption",
-                        Description = "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-                        Year = 1994,
-                        Genre = "Drama",
+                        Title = "Jurassic Park",
+                        Description = "During a preview tour, a theme park suffers a major power breakdown that allows its cloned dinosaur exhibits to run amok.",
+                        Year = 1990,
+                        Genre = "Action, Adventure",
                         Rating = 4.9,
                         PosterUrl = "/Content/Images/Movies/Movie_poster_4.png",
-                        VideoUrl = "/Content/Videos/sample.mp4"
+                        VideoUrl = "/Content/Videos/jurassic_park.mp4"
                     },
                     new Movie
                     {
-                        Title = "Interstellar",
-                        Description = "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival .",
-                        Year = 2014,
-                        Genre = "Adventure, Drama, Sci-Fi",
+                        Title = "Star Wars",
+                        Description = "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station.",
+                        Year = 1977,
+                        Genre = "Sci-Fi, Action",
                         Rating = 4.6,
                         PosterUrl = "/Content/Images/Movies/Movie_poster_5.png",
-                        VideoUrl = "/Content/Videos/sample.mp4"
+                        VideoUrl = "/Content/Videos/star_wars.mp4"
                     }
                 );
             }
+
+            // Always update existing movies to correct titles, genres, and descriptions based on PosterUrl
+            var movie1 = context.Movies.FirstOrDefault(m => m.PosterUrl == "/Content/Images/Movies/Movie_poster_1.png");
+            if (movie1 != null)
+            {
+                movie1.Title = "Alien";
+                movie1.Genre = "Action, Adventure, Horror";
+                movie1.Description = "In deep space, the crew of the commercial starship Nostromo is awakened from their cryo-sleep capsules halfway through their journey home to investigate a distress call from an alien vessel.";
+            }
+            var movie2 = context.Movies.FirstOrDefault(m => m.PosterUrl == "/Content/Images/Movies/Movie_poster_2.png");
+            if (movie2 != null)
+            {
+                movie2.Title = "Back to the Future";
+                movie2.Genre = "Sci-Fi, Adventure";
+                movie2.Description = "Marty McFly, a 17-year-old high school student, is accidentally sent thirty years into the past in a time-traveling DeLorean invented by his close friend, the maverick scientist Doc Brown.";
+            }
+            var movie3 = context.Movies.FirstOrDefault(m => m.PosterUrl == "/Content/Images/Movies/Movie_poster_3.png");
+            if (movie3 != null)
+            {
+                movie3.Title = "The Goonies";
+                movie3.Genre = "Adventure, Comedy";
+                movie3.Description = "A group of young misfits called The Goonies discover an ancient map and set out on an adventure to find a legendary pirate's long-lost treasure.";
+            }
+            var movie4 = context.Movies.FirstOrDefault(m => m.PosterUrl == "/Content/Images/Movies/Movie_poster_4.png");
+            if (movie4 != null)
+            {
+                movie4.Title = "Jurassic Park";
+                movie4.Genre = "Action, Adventure";
+                movie4.Description = "During a preview tour, a theme park suffers a major power breakdown that allows its cloned dinosaur exhibits to run amok.";
+            }
+            var movie5 = context.Movies.FirstOrDefault(m => m.PosterUrl == "/Content/Images/Movies/Movie_poster_5.png");
+            if (movie5 != null)
+            {
+                movie5.Title = "Star Wars";
+                movie5.Genre = "Sci-Fi, Action";
+                movie5.Description = "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station.";
+            }
+            context.SaveChanges();
         }
     }
 }
